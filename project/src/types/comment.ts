185a99d0ef0bebbf29ac1786d5d52bbee7b81655
Alexander Golubenko <webgolub@gmail.com>
2023-03-16 14:39:08 +0300

    Добавит типы и моки офферов и комментариев

---
+++ project/src/types/comment.ts
@@ -0,0 +1,14 @@
+export type User ={
+  avatarUrl: string;
+  id: number;
+  isPro: boolean;
+  name: string;
+};
+
+export type Comment = {
+  comment: string;
+  date: Date;
+  id: number;
+  rating: number;
+  user: User;
+};
