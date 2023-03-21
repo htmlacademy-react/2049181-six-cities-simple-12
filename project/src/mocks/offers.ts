96b268f1800eab3a4028fef056e6102e779bfd1e
Alexander Golubenko <webgolub@gmail.com>
2023-03-21 17:47:49 +0300

    Изменит координаты для моковых данных

--- project/src/mocks/offers.ts
+++ project/src/mocks/offers.ts
@@ -3,10 +3,10 @@ import { Offer } from '../types/offer';
 export const offers: Offer[] = [
   {
     city: {
-      name: 'Paris',
+      name: 'Amsterdam',
       location: {
-        latitude: 48.85661,
-        longitude: 2.351499,
+        latitude: 52.377956,
+        longitude: 4.897070,
         zoom: 13
       }
     },
@@ -45,18 +45,18 @@ export const offers: Offer[] = [
     },
     description: 'Discover daily local life in city center, friendly neighborhood, clandestine casino, karaoke, old-style artisans, art gallery and artist studio downstairs.',
     location: {
-      latitude: 48.862610000000004,
-      longitude: 2.369499,
+      latitude: 52.3909553943508,
+      longitude: 4.85309666406198,
       zoom: 16
     },
     id: 1
   },
   {
     city: {
-      name: 'Cologne',
+      name: 'Amsterdam',
       location: {
-        latitude: 50.938361,
-        longitude: 6.959974,
+        latitude: 52.377956,
+        longitude: 4.897070,
         zoom: 13
       }
     },
@@ -104,18 +104,18 @@ export const offers: Offer[] = [
     },
     description: 'A new spacious villa, one floor. All commodities, jacuzzi and beautiful scenery. Ideal for families or friends.',
     location: {
-      latitude: 50.945361,
-      longitude: 6.962974,
+      latitude: 52.3609553943508,
+      longitude: 4.85309666406198,
       zoom: 16
     },
     id: 2
   },
   {
     city: {
-      name: 'Dusseldorf',
+      name: 'Amsterdam',
       location: {
-        latitude: 51.225402,
-        longitude: 6.776314,
+        latitude: 52.377956,
+        longitude: 4.897070,
         zoom: 13
       }
     },
@@ -162,18 +162,18 @@ export const offers: Offer[] = [
     },
     description: 'I rent out a very sunny and bright apartment only 7 minutes walking distance to the metro station. The apartment has a spacious living room with a kitchen, one bedroom and a bathroom with mit bath. A terrace can be used in summer.',
     location: {
-      latitude: 51.217402,
-      longitude: 6.7693140000000005,
+      latitude: 52.3909553943508,
+      longitude: 4.929309666406198,
       zoom: 16
     },
     id: 3
   },
   {
     city: {
-      name: 'Brussels',
+      name: 'Amsterdam',
       location: {
-        latitude: 50.846557,
-        longitude: 4.351697,
+        latitude: 52.377956,
+        longitude: 4.897070,
         zoom: 13
       }
     },
@@ -218,8 +218,8 @@ export const offers: Offer[] = [
     },
     description: 'Design interior in most sympathetic area! Complitely renovated, well-equipped, cosy studio in idyllic, over 100 years old wooden house. Calm street, fast connection to center and airport.',
     location: {
-      latitude: 50.865556999999995,
-      longitude: 4.371696999999999,
+      latitude: 52.3809553943508,
+      longitude: 4.939309666406198,
       zoom: 16
     },
     id: 4
